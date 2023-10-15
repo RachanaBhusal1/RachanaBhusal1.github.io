78a09c3c3ada064c156de0741990ca967534bf29
Rachana Bhusal <rachanabhusal99@gmail.com>
2023-10-15 12:24:56 +1100

    changes done in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 
 import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
-import Menu from './pages/menu/Menu';
-import AboutUs from './pages/aboutus/Aboutus';
-import Contactus from './pages/contactus/Contactus';
-import{BrowserRouter,Routes, Route} from 'react-router-dom';
 function App() {
   return (
-    <BrowserRouter>
-     <Navbar/>
-    <Routes>
-      <Route path ="/" element={<Home/>}/>
-      <Route path ="/Menu" element={<Menu/>}/>
-      <Route path ="/Aboutus" element={<AboutUs/>}/>
-      <Route path ="/Contactus" element={<Contactus/>} />
-  
-      </Routes>
-      </BrowserRouter>
+   <></>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Restaurant Website/i);
+  const linkElement = screen.getByText(/Rachana Portfolio/i);
   expect(linkElement).toBeInTheDocument();
 });
