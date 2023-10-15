8e953342bf79c372de2497ef490f554e09131be1
Rachana Bhusal <rachanabhusal99@gmail.com>
2023-10-15 21:41:12 +1100

    addidng information in about.js

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const aboutus = () =>{
+    return(
+       
+        <div className="aboutus">
+            <h2>Introduction </h2>
+            <p>Hello, and welcome to my portfolio. My name is Rachana Bhusal, and I am a IT Student with a passion for Web Development. Over the years, I have dedicated myself to honing my skills and knowledge to become a proficient professional in my field. In this portfolio, I will provide you with an overview of my educational background, work experience, and some of the significant projects I have had the privilege of being a part of.</p>
+            
+        </div>
+    )
+}
+
+export default aboutus;
