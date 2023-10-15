cac74d462b197c7ad94ae7f6f805c06d05de0863
Rachana Bhusal <rachanabhusal99@gmail.com>
2023-10-15 22:18:46 +1100

    changes done in about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React from 'react';
-const aboutus = () =>{
+function about(){
     return(
        
-        <div className="aboutus">
+        <div>
             <h2>Introduction </h2>
             <p>Hello, and welcome to my portfolio. My name is Rachana Bhusal, and I am a IT Student with a passion for Web Development. Over the years, I have dedicated myself to honing my skills and knowledge to become a proficient professional in my field. In this portfolio, I will provide you with an overview of my educational background, work experience, and some of the significant projects I have had the privilege of being a part of.</p>
             
@@ -10,4 +10,4 @@ const aboutus = () =>{
     )
 }
 
-export default aboutus;
+export default about;
