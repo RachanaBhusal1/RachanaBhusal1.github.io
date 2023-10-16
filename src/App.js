700dba8a68f737f0ebe40b9ecf29a99a459eb6d0
Rachana Bhusal <rachanabhusal99@gmail.com>
2023-10-16 14:09:15 +1100

    delected and try to attend again

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import './components/Style.css';
-import Navbar from './components/Navbar';
-import About from './components/about';
-import Project from './components/Project';
-import Contact from './components/Contact';
-import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-        <Route path="/about" component={<About/>} />
-        <Route path="/project" component={<Project/>} />
-        <Route path="/contact" component={<Contact/>} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+    <h1>Rachana Bhusal</h1>
+        </BrowserRouter>
   );
 }
 
