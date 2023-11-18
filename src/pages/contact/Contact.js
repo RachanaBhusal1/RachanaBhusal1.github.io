1387f3e44dcec2a167bc79d7521fb3f30e9e38f9
Rachana Bhusal <rachanabhusal99@gmail.com>
2023-11-18 21:40:18 +1100

    changes done in contact.js

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -8,8 +8,15 @@ const Contact = () =>{
       <p>Name:Rachana Bhusal</p>
       <p>Email: rachanabhusal99@gmail.com</p>
       <p>Phone: 0450819742</p>
+      <div className="resume">
+        <h2>Resume</h2>
+        <a href="/file:///C:/Users/Sabin/Desktop/Rachana%20Bhusal%20Aged%20Care%20Resume.pdf" target="_blank" rel="noopener noreferrer">
+          Download Resume
+        </a>
+      </div>
       {/* Add more contact information as needed */}
     </div>
+    
   )
 }
 
