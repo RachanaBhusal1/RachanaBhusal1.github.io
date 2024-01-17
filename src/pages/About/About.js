520f93210dbd63f3ea7da6538cb06755e0e1057f
Rachana Bhusal <rachanabhusal99@gmail.com>
2024-01-17 21:04:09 +1100

    Changes done in about.js and about.css

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,41 +6,22 @@ import Rachana from './image/Rachana.jpg';
 const About = () =>{
     return(
        
-        <div  id='about'className='about-container'>
-            <h2>About Rachana</h2>
-            <div className='profile-container'>
-        <img src={Rachana} alt='Profile' className='profile-image'/>
-      </div>
-      <span className='text-container'>
-        Welcome to my portfolio! I'm Rachana Bhusal, a passionate and versatile
-        developer with a love for creating innovative solutions. My journey in
-        the world of web development began 3 years ago, and since then, I have
-        been dedicated to honing my skills and staying at the forefront of
-        technology.
+        <div className='about-container'>
+            <h2>About Me</h2>
+    
+         <img src={Rachana} alt='Profile' className='profile-image'/> 
+  <span>
+        Welcome to my portfolio! I'm Rachana Bhusal, graduate with a degree in 
+        Information Technology. My Academic journey equipped me with a strong
+        foundation in web developement, focusing on HTML, CSS and JavaScript.
       </span>
-      <span className='text-container'>
-        My expertise lies in building web applications using cutting-edge
-        technologies. I specialize in front-end development, with a strong
-        command of React.js and its ecosystem.
+      <span>I've worked on personal projects, like Restaurant website, to build
+        a portfolio. I'm passionate about creating user-friendly websites and
+        excited about contributing my skills to innovative projects.
       </span>
-      <span className='text-container'>
-        Over the years, I have had the privilege of working on diverse projects
-        that have equipped me with a broad skill set. From collaborating on
-        cross-functional teams to tackling complex problems, I thrive in dynamic
-        environments that challenge me to push my boundaries.
-      </span>
-      <span className='text-container'>
-        Continuous learning is a core value of mine, and I am always eager to
-        explore new technologies and frameworks. My commitment to delivering
-        high-quality code, combined with a keen eye for detail, ensures that
-        every project I undertake is a success.
-      </span>
-      <span className='text-container'>
-        Thank you for visiting my portfolio. Feel free to explore my projects
-        and get in touch if you have any questions or exciting opportunities. I
-        look forward to connecting with like-minded individuals and
-        contributing my skills to create meaningful digital experiences.
-      </span>
+      <span>Despite being new to the field, I bring enthusiasm, a willingness 
+        to learn, and a solid understanding of web development principle.</span>
+        
 
         </div>
     )
