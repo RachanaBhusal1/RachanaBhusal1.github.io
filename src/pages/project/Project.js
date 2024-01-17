d1ac504852911fb872205653fbef3f80eab1edf7
Rachana Bhusal <rachanabhusal99@gmail.com>
2024-01-17 22:46:34 +1100

    Changes done in Project.js and Project.css

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -5,10 +5,18 @@ const Project= () => {
   return (
     <div className="project-container">
       <h2>Restaurant Website</h2>
-      <p>"Designed a cool website for a nearby restaurant!
- Made it easy to use and look awesome.
-  Now people can check out the menu and more online.
-  It's like bringing the restaurant vibe to the internet!"</p>
+      <p>Project: Restaurant website</p>
+      <p>	Project Link: (Link to be added)</p> 
+      <p>Technology used: Bootstrap, React</p>
+      <p>Features: Homepage, Contact us, About, Menu</p>
+      <p>Role: Design, Develop and Deploy and update content.</p>
+
+      <p>This project showcases a dynamic and visually appealing platform developed
+         using Bootstrap and React frameworks. In this project, my role involves the
+         comprehensive design, deployment, and continuous content updates, ensuring
+          a seamless and engaging online experience for users. Used CI-CD technique
+           to deploy project to GitHub.</p>
+
       { /* <div className="code-section">
       <a href="http://localhost:3002/" target="_blank" rel="noopener noreferrer">
   Restaurant Website
