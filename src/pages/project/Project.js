7bb1246d2949ab79b87fdddfbee2f549c20556d7
Rachana Bhusal <rachanabhusal99@gmail.com>
2024-01-17 22:51:02 +1100

    Changes done in Project.js

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -11,11 +11,11 @@ const Project= () => {
       <p>Features: Homepage, Contact us, About, Menu</p>
       <p>Role: Design, Develop and Deploy and update content.</p>
 
-      <p>This project showcases a dynamic and visually appealing platform developed
+      <p><i>This project showcases a dynamic and visually appealing platform developed
          using Bootstrap and React frameworks. In this project, my role involves the
          comprehensive design, deployment, and continuous content updates, ensuring
           a seamless and engaging online experience for users. Used CI-CD technique
-           to deploy project to GitHub.</p>
+           to deploy project to GitHub.</i></p>
 
       { /* <div className="code-section">
       <a href="http://localhost:3002/" target="_blank" rel="noopener noreferrer">
